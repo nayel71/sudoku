40746ee337a09da19477dd63e11dcf1680898064
Samin Riasat <nayel71@gmail.com>
2020-06-22 01:23:41 -0400

    removed globals

--- sudoku.c
+++ sudoku.c
@@ -9,10 +9,7 @@
 
 bool DISPLAY_OUTPUT = false;
 
-int number_of_assignments = 0;
-
 // valid_values[x][y][z] indicates if z is a valid value for square (x, y) on the board
-bool valid_values[N][N][N];
 
 // reads a NxN board
 void read(int board[N][N]) {
@@ -34,7 +31,7 @@ void print(int board[N][N]) {
 }
 
 // initialises the array of valid values
-void initialise_valid_values(void) {
+void initialise(bool valid_values[N][N][N]) {
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
 			for (int k = 0; k < N; k++) {
@@ -45,7 +42,7 @@ void initialise_valid_values(void) {
 }
 
 // returns the number of valid values for square (x, y)
-int remaining_values(int x, int y) {
+int remaining_values(bool valid_values[N][N][N], int x, int y) {
 	int count = 0;
 	for (int i = 0; i < N; i++) {
 		if (valid_values[x][y][i]) count++;
@@ -53,8 +50,8 @@ int remaining_values(int x, int y) {
 	return count;
 }
 
-// updates the array of valid values as per the value in square (x, y) on board
-void update_valid_values(int board[N][N], int x, int y) {
+// updates valid values as per the value in square (x, y) on board
+void update(bool valid_values[N][N][N], int board[N][N], int x, int y) {
 	if (board[x][y]) {
 		for (int i = 0; i < N; i++) {
 			if (i != x) valid_values[i][y][board[x][y] - 1] = false;
@@ -84,19 +81,19 @@ int degree(int board[N][N], int x, int y) {
 }
 
 // finds an empty cell with the fewest valid values, stores its coordinates at x and y
-bool find_best_empty_cell(int board[N][N], int *x, int *y) {
+bool find_best_empty_cell(int board[N][N], bool valid_values[N][N][N], int *x, int *y) {
 	int current_min = N;
 	*x = -1;
 	*y = -1;
 	bool empty_found = false;
 	for (int j = 0; j < N; j++) {
 		for (int i = 0; i < N; i++) {
-			if (!board[i][j] && remaining_values(i, j) < current_min) { // MRV heuristic
-				current_min = remaining_values(i, j);
+			if (!board[i][j] && remaining_values(valid_values, i, j) < current_min) { // MRV heuristic
+				current_min = remaining_values(valid_values, i, j);
 				*x = i;
 				*y = j;
 				empty_found = true;
-			} else if (!board[i][j] && remaining_values(i, j) == current_min) { // break tie
+			} else if (!board[i][j] && remaining_values(valid_values, i, j) == current_min) { // break tie
 				if (degree(board, i, j) < degree(board, *x, *y)) { // degree heuristic
 					*x = i;
 					*y = j;
@@ -109,8 +106,8 @@ bool find_best_empty_cell(int board[N][N], int *x, int *y) {
 }
 
 // finds the value for (x, y) that rules out the fewest values for the neighbouring unassigned variables
-bool best_value(int board[N][N], int x, int y, int *ans) { // least constraining value heuristic
-	int max_count = N * N;//(N - 1 + N - 1 + N - 1) * N;
+bool best_value(int board[N][N], bool valid_values[N][N][N], int x, int y, int *ans) { // least constraining value heuristic
+	int max_count = N * N;
 	bool retval = false;
 	for (int value = 0; value < N; value++) {
 		if (valid_values[x][y][value]) {
@@ -136,14 +133,14 @@ bool best_value(int board[N][N], int x, int y, int *ans) { // least constraining
 }
 
 // solves the board
-bool solve(int board[N][N]) {
+bool solve(int board[N][N], bool valid_values[N][N][N], int *trials) {
 	int x, y, value;
-	if (!find_best_empty_cell(board, &x, &y)) {
+	if (!find_best_empty_cell(board, valid_values, &x, &y)) {
 		return true;
 	}
-	while (best_value(board, x, y, &value)) {
+	while (best_value(board, valid_values, x, y, &value)) {
 		board[x][y] = value + 1;
-		number_of_assignments++;
+		(*trials)++;
 		if (DISPLAY_OUTPUT) {
 			printf("Assigned %d to (%d, %d):\n", value + 1, x + 1, y + 1);
 			print(board);
@@ -151,8 +148,8 @@ bool solve(int board[N][N]) {
 		// save valid values in case of failure
 		bool backup[N][N][N];
 		memcpy(backup, valid_values, sizeof(backup));
-		update_valid_values(board, x, y);
-		if (solve(board)) { // success!
+		update(valid_values, board, x, y);
+		if (solve(board, valid_values, trials)) { // success!
 			return true;
 		} else { // fail :(
 			if (DISPLAY_OUTPUT) {
@@ -170,22 +167,24 @@ int main(int argc, char **argv) {
 	if (argc == 2 && argv[1][0] == '-' && argv[1][1] == 'o' && argv[1][2] == '\0') {
 		DISPLAY_OUTPUT = true;
 	} else if (argc > 1) {
-		fprintf(stderr, "Invalid option: %s\nUsage: %s [-o] < sudoku.txt\n", argv[1], argv[0]);
+		fprintf(stderr, "Usage: %s [-o] < sudoku.txt\n", argv[0]);
 		return EXIT_FAILURE;
 	}
 	clock_t begin = clock();
 	int board[N][N];
+	bool valid_values[N][N][N];
+	int trials = 0;
 	read(board);
 	print(board);
-	initialise_valid_values();
+	initialise(valid_values);
 	for (int y = 0; y < N; y++) {
 		for (int x = 0; x < N; x++) {
-			update_valid_values(board, x, y);
+			update(valid_values, board, x, y);
 		}
 	}
-	if (solve(board)) {
+	if (solve(board, valid_values, &trials)) {
 		if (DISPLAY_OUTPUT) {
-			printf("Status:\tSuccess!\nTrials:\t%d\n", number_of_assignments);
+			printf("Status:\tSuccess!\nTrials:\t%d\n", trials);
 		} else {
 			printf("Solution:\n");
 			print(board);
